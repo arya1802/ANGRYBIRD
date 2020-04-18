b269d8c44b9bda197f281d2f6552595fe7e11990
arya1802 <59442589+arya1802@users.noreply.github.com>
2020-04-18 15:26:58 +0530

    Add files via upload

---
+++ angryBirdsStage8.5-master/sketch.js
@@ -0,0 +1,104 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "bg2.jpg";
+var score = 0;
+
+function preload() {
+   
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(920,120,70,70);
+    log4 = new Log(810,100,300, PI/2);
+    box6 = new Box(700,120,70,70);
+
+    bird = new Bird(200,50);
+    pig2 = new Pig(810, 480);
+        //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+   
+        background("black");
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    box6.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();  
+    pig2.display();
+    pig2.score();  
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        bird.trajectory=[];
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 550});
+       slingshot.attach(bird.body);
+    }
+}
+
